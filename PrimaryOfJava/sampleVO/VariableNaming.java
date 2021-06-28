c7e0ab83908beed0631f62c5fa84d6a32bd8bba9
jjwon915 <jjwon915@naver.com>
2021-06-28 23:45:58 +0900

    naming : camelCase

---
+++ PrimaryOfJava/sampleVO/VariableNaming.java
@@ -0,0 +1,16 @@
+package sampleVO;
+
+public class VariableNaming {
+
+	public static void main(String[] args) {
+		
+		// camel case로 표기한다.
+		int humanResourceDepartmentDocumentCount = 10;	
+
+	}
+
+	// method도 마찬가지로 camelCase로 표기한다.
+	public void calculateSalary() {
+		
+	}
+}
