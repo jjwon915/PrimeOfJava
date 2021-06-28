51e373720ff8a8bf86c530f237dbc2f423579439
jjwon915 <jjwon915@naver.com>
2021-06-28 23:59:39 +0900

    Shift 연산자.

---
+++ PrimaryOfJava/primary/Calc.java
@@ -0,0 +1,23 @@
+package primary;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		
+		// shift ¿¬»êÀÚ
+		int first = 10;
+		int second = 2;
+		first = second >> 2;
+		System.out.println(first);
+		
+		second = 2;
+		first = second << 2;
+		System.out.println(first);
+		
+		second = 2;
+		first = second << 1;
+		System.out.println(first);
+
+	}
+
+}
