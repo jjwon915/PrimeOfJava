f9c16ca9c0858233fce5ff6a0f5877484cdc45b6
jjwon915 <jjwon915@naver.com>
2021-06-28 23:45:35 +0900

    casting, vo생성 및 사용

---
+++ PrimaryOfJava/sampleVO/Casting.java
@@ -0,0 +1,17 @@
+package sampleVO;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double dbValue = 3.12341234;
+		int intValue = (int) dbValue;
+		System.out.println("intValue : " + intValue);
+		
+		int intValue1 = 3;
+		double dbValue1 = (double) intValue;
+		System.out.println(dbValue1);
+		
+	}
+
+}
---
+++ PrimaryOfJava/sampleVO/SampleVO.java
@@ -0,0 +1,15 @@
+package sampleVO;
+
+public class SampleVO {
+
+	public boolean sampleBoolean;
+	public char sampleChar;
+	public String sampleString;
+	public byte sampleByte;
+	public short sampleShort;
+	public int sampleInt;
+	public long sampleLong;
+	public float sampleFloat;
+	public double sampleDouble;
+	
+}
